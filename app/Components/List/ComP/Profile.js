e6d76e58d6b953e3f7eb91a89f7009e3aa4077e7
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-02-15 14:54:57 +0530

    auth-done

--- app/Components/List/ComP/Profile.js
+++ app/Components/List/ComP/Profile.js
@@ -1,4 +1,16 @@
+"use client"
+
+import {useSession} from "next-auth/react"
+import { getServerSession } from "next-auth"
+import { useEffect } from "react";
+
 export default function Profile(){
+
+    const {data:session} =useSession();
+    
+    useEffect(()=>{
+        console.log(session);
+    },[session]);
     return (
         <div id="profile-container">
             <div id="profile-pic">
@@ -6,7 +18,7 @@ export default function Profile(){
             </div>
 
             <div id="profile-name">
-                Nihal Rahman
+                {session?.user?.name}
             </div>
 
         </div>
--- app/Components/List/ComP/sidebar.js
+++ app/Components/List/ComP/sidebar.js
@@ -1,9 +1,10 @@
 import Group from "./groups"
+import {signIn,signOut,useSession} from "next-auth/react"
 export default function Sidebar(){
     return(
         <div id="side-container">
             <Group></Group>
-            
+        <button id="sign-out" onClick={()=>signOut({callbackUrl:"/"})}>Logout</button> 
         </div>
     )
 }
