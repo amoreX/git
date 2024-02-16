6fcf2fd55089ba800588df2cb1cc1a7da1669ae7
WahidX <wahidx93@gmail.com>
2024-02-17 02:15:32 +0530

    burger almost done

--- app/Components/List/page.js
+++ app/Components/List/page.js
@@ -1,31 +1,34 @@
-"use client"
+"use client";
 
-import {signIn,signOut,useSession} from "next-auth/react"
-import { getServerSession } from "next-auth"
-import "./list.css"
-import Sidebar from "./ComP/sidebar"
-import Profile from "./ComP/Profile"
-import Body from "./ComP/body"
-import Add from "./ComP/Add"
-import { useEffect } from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import "./list.css";
+import Sidebar from "./ComP/sidebar";
+import Profile from "./ComP/Profile";
+import Body from "./ComP/body";
+import Add from "./ComP/Add";
+import Burger from "./ComP/Burger";
+import { useState, useEffect } from "react";
 
-export default   function List(){
-    const {data:session} =useSession();
-    // const session = await getServerSession();
-    useEffect(()=>{
-        console.log(session);
-    },[session]);
+export default function List() {
+	const { data: session } = useSession();
+	useEffect(() => {
+		console.log(session);
+	}, [session]);
 
-    return(
-        <div id="main">
-           
-            <Sidebar></Sidebar>
-            <Body></Body>
-            <Profile></Profile>
-            <Add></Add>
+	const [ismobile, setIsmobile] = useState(true);
 
+	const handleIsMobile = () => {
+		setIsmobile(!ismobile);
+	};
 
-
-        </div>
-    )
+	return (
+		<div id="main">
+			<Burger check={ismobile} mobile={handleIsMobile}></Burger>
+			<Sidebar check={ismobile} mobile={handleIsMobile}></Sidebar>
+			<Body></Body>
+			<Profile></Profile>
+			<Add></Add>
+		</div>
+	);
 }
