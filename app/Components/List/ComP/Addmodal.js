3a6a584f930e5fa6bc0d54f62a28092254adbf1d
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-02-17 22:13:37 +0530

    done modal

--- app/Components/List/ComP/Addmodal.js
+++ app/Components/List/ComP/Addmodal.js
@@ -1,56 +1,64 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion,AnimatePresence } from "framer-motion";
 
 export default function Modal({ check, set }) {
 
 	const handleClick=()=>{
 		set();
 	}
-	return check == true ? (
-		<div id="modal-original">
-			
-		<motion.div
-			id="modal-container"
-			initial={{
-				opacity: 0,
-				scale: 0.1,
-			}}
-			transition={{
-				type: "tween",
-				ease: "easeInOut",
-				duration: 0.38,
-			}}
-			animate={{
-				opacity: 1,
-				scale: 1,
-			}}
-			onClick={()=>handleClick()}
-		></motion.div>
-			<motion.div
-				id="modal"
+	return (
+		<AnimatePresence>
+		  {check && (
+			<div id="modal-original">
+			  <motion.div
+				id="modal-container"
 				initial={{
-					opacity:0,
-					y: 500,
+				  opacity: 0,
+				  scale: 0.1,
 				}}
 				transition={{
-					delay: 0.5,
-					type: "tween",
-					ease: "easeInOut",
-					duration: 0.74,
+				  type: "tween",
+				  ease: "easeInOut",
+				  duration: 0.38,
 				}}
 				animate={{
-					opacity:1,
-					y: 0,
+				  opacity: 1,
+				  scale: 1,
 				}}
-			>
-
+				exit={{
+				  opacity: 0,
+				  scale: 0.1,
+				}}
+				onClick={() => handleClick()}
+			  ></motion.div>
+	
+			  <motion.div
+				id="modal"
+				initial={{
+				  opacity: 0,
+				  y: 500,
+				}}
+				transition={{
+				  delay: 0.1,
+				  type: "tween",
+				  ease: "easeInOut",
+				  duration: 0.74,
+				}}
+				animate={{
+				  opacity: 1,
+				  y: 0,
+				}}
+				exit={{
+				  opacity: 0,
+				  y: 500,
+				}}
+			  >
 				<input type="text" placeholder="Enter task"></input>
 				<div id="button">Add</div>
-			</motion.div>
-		
-		</div>
-	) : (
-		<div></div>
-	);
+			  </motion.div>
+			</div>
+		  )}
+		</AnimatePresence>
+	  );
 }
