283de991e0f55c988bd47be51497bf5af86fcd6f
WahidX <wahidx93@gmail.com>
2024-02-19 21:34:19 +0530

    perma sesion

--- app/page.js
+++ app/page.js
@@ -2,12 +2,13 @@
 
 import Login from "./Components/Login/login";
 import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import { useRouter } from "next/router";
 export default function Home() {
-	const router = useRouter();
+	// const router = useRouter();
 	const { data: session } = useSession();
 	if (session && session.user) {
-		router.push("/Components/List");
+		redirect("/Components/List");
 	}
 
 	return (
